dedfb483211739b3c0a6b40c2192ed66bbf13721
twatson83 <tswatson123@gmail.com>
2015-02-24 16:12:47 +0000

    Added contact page

--- R.MessageBus.PublicWebsite/Controllers/HomeController.cs
+++ R.MessageBus.PublicWebsite/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Net.Mail;
+using System.Web.Configuration;
+using System.Web.Mvc;
 
 namespace R.MessageBus.PublicWebsite.Controllers
 {
@@ -24,7 +26,43 @@ namespace R.MessageBus.PublicWebsite.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("rmessagebus@gmail.com");
+
+                var emails = WebConfigurationManager.AppSettings["Emails"].Split(',');
+                foreach (var email in emails)
+                {
+                    mail.To.Add(email);
+                }
+                mail.Subject = model.Subject;
+                mail.Body = model.Message;
+
+                smtpServer.Port = 587;
+                smtpServer.Credentials = new System.Net.NetworkCredential(WebConfigurationManager.AppSettings["EmailUsername"], WebConfigurationManager.AppSettings["EmailPassword"]);
+                smtpServer.EnableSsl = true;
+
+                smtpServer.Send(mail);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
         public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult Monitor()
         {
             return View();
         }
