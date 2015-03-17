6597029fd0ba3770d0d0acae32194180028cbf0d
twatson83 <tswatson123@gmail.com>
2015-03-17 10:04:46 +0000

    Added Audit Monitor screenshots

--- R.MessageBus.PublicWebsite/App_Start/RouteConfig.cs
+++ R.MessageBus.PublicWebsite/App_Start/RouteConfig.cs
@@ -14,6 +14,17 @@ namespace R.MessageBus.PublicWebsite
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Installer",
+                url: "downloads/installer",
+                defaults: new { controller = "Downloads", action = "installer" }
+            );
+            routes.MapRoute(
+                name: "Release",
+                url: "downloads/release",
+                defaults: new { controller = "Downloads", action = "release" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
